21eef84d3c576c2d4e1bae0c7dfa9f47fdd9550f
SonicPrCr <mdavydov@gmail.com>
2024-10-08 11:00:25 +0900

    Add Desktop and fix burger

--- main.js
+++ main.js
@@ -2,10 +2,6 @@ const burger = document.querySelector(".burger");
 const close__button = document.querySelector(".close__button");
 const menu__overlay = document.querySelector(".menu");
 
-console.log("@", close__button);
-
-console.log("@", menu__overlay);
-
 burger.addEventListener("click", () => {
   menu__overlay.classList.add("overlay__open");
 
